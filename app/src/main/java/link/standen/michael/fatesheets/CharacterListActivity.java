c9450f64695fd97b933e116eab0de0495696f8ff
Michael Standen <screaminghawk@gmail.com>
2017-04-06 18:38:26 +1200

    Renamed layout files

--- app/src/main/java/link/standen/michael/fatesheets/CharacterListActivity.java
+++ app/src/main/java/link/standen/michael/fatesheets/CharacterListActivity.java
@@ -14,7 +14,7 @@ public class CharacterListActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_character_list);
+		setContentView(R.layout.character_list_activity);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
