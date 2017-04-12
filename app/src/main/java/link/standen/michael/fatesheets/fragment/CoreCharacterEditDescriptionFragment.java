5055b8438c1a06ff4d48361276d5c0eba707accc
Michael Standen <screaminghawk@gmail.com>
2017-04-13 11:22:24 +1200

    Description tab fields update on key press

--- app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditDescriptionFragment.java
+++ app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditDescriptionFragment.java
@@ -1,9 +1,11 @@
 package link.standen.michael.fatesheets.fragment;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import link.standen.michael.fatesheets.R;
 
@@ -16,6 +18,23 @@ public class CoreCharacterEditDescriptionFragment extends CoreCharacterEditAbstr
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.core_character_edit_description, container, false);
+
+		// Update fields on key press
+		rootView.findViewById(R.id.name).setOnKeyListener(new View.OnKeyListener(){
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				getCharacter().setName(((TextView)v).getText().toString());
+				return false;
+			}
+		});
+		rootView.findViewById(R.id.description).setOnKeyListener(new View.OnKeyListener(){
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				getCharacter().setDescription(((TextView)v).getText().toString());
+				return false;
+			}
+		});
+
 		return rootView;
 	}
 }
