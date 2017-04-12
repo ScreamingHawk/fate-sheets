456eb24cb2e2f297c5d94af1b5738bb1e17288c6
Michael Standen <screaminghawk@gmail.com>
2017-04-13 11:46:18 +1200

    Stunts tab details correctly persist

--- app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStuntsFragment.java
+++ app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStuntsFragment.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import link.standen.michael.fatesheets.R;
+import link.standen.michael.fatesheets.activity.CoreCharacterEditActivity;
+import link.standen.michael.fatesheets.adapter.DeletableStringArrayAdapter;
+import link.standen.michael.fatesheets.model.CoreCharacter;
 
 /**
  * A fragment for managing a characters stunts.
@@ -16,6 +20,35 @@ public class CoreCharacterEditStuntsFragment extends CoreCharacterEditAbstractFr
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.core_character_edit_stunts, container, false);
+
+		final CoreCharacter character = getCharacter();
+
+		// Stunts
+		final DeletableStringArrayAdapter stuntListAdapter = new DeletableStringArrayAdapter((CoreCharacterEditActivity) getContext(),
+				R.layout.core_character_edit_stunts_list_item, getCharacter().getStunts());
+		((ListView) rootView.findViewById(R.id.stunts_list)).setAdapter(stuntListAdapter);
+
+		rootView.findViewById(R.id.add_stunt).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				character.getStunts().add("");
+				stuntListAdapter.notifyDataSetChanged();
+			}
+		});
+
+		// Extras
+		final DeletableStringArrayAdapter extraListAdapter = new DeletableStringArrayAdapter((CoreCharacterEditActivity) getContext(),
+				R.layout.core_character_edit_extras_list_item, getCharacter().getExtras());
+		((ListView) rootView.findViewById(R.id.extras_list)).setAdapter(extraListAdapter);
+
+		rootView.findViewById(R.id.add_extra).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				character.getExtras().add("");
+				stuntListAdapter.notifyDataSetChanged();
+			}
+		});
+
 		return rootView;
 	}
 }
