b0e6e9691974a5bf409ec87d03f4932964ec981c
Michael Standen <screaminghawk@gmail.com>
2017-04-13 15:35:10 +1200

    Added stress

---
+++ app/src/main/java/link/standen/michael/fatesheets/adapter/StressArrayAdapter.java
@@ -0,0 +1,93 @@
+package link.standen.michael.fatesheets.adapter;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+import link.standen.michael.fatesheets.R;
+import link.standen.michael.fatesheets.activity.CoreCharacterEditActivity;
+import link.standen.michael.fatesheets.model.Stress;
+
+/**
+ * Manages a list of character stress.
+ */
+public class StressArrayAdapter extends ArrayAdapter<Stress> {
+
+	private static final String TAG = StressArrayAdapter.class.getName();
+
+	private final CoreCharacterEditActivity context;
+	private final int resourceId;
+	private final List<Stress> items;
+
+	public StressArrayAdapter(@NonNull CoreCharacterEditActivity context, @LayoutRes int resourceId, @NonNull List<Stress> items) {
+		super(context, resourceId, items);
+
+		this.context = context;
+		this.resourceId = resourceId;
+		this.items = items;
+	}
+
+	public Stress getItem(int index){
+		return items.get(index);
+	}
+
+	@NonNull
+	@Override
+	public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+		View view = convertView;
+		if (view == null){
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(resourceId, null);
+		}
+
+		view.findViewById(R.id.delete).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				items.remove(position);
+				StressArrayAdapter.this.notifyDataSetChanged();
+			}
+		});
+
+		final Stress item = getItem(position);
+
+		// Value
+		TextView valueView = ((TextView)view.findViewById(R.id.value));
+		if (item.getValue() != null) {
+			valueView.setText(item.getValue().toString());
+		}
+		valueView.setOnKeyListener(new View.OnKeyListener(){
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				try {
+					item.setValue(Integer.parseInt(((TextView)v).getText().toString()));
+				} catch (NumberFormatException e){
+					item.setValue(0);
+				}
+				return false;
+			}
+		});
+
+		// Active
+		CheckBox activeView = ((CheckBox)view.findViewById(R.id.active));
+		activeView.setChecked(item.getActive());
+		activeView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				item.setActive(isChecked);
+			}
+		});
+
+		return view;
+	}
+}
--- app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStressFragment.java
+++ app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStressFragment.java
@@ -9,8 +9,10 @@ import android.widget.ListView;
 import link.standen.michael.fatesheets.R;
 import link.standen.michael.fatesheets.activity.CoreCharacterEditActivity;
 import link.standen.michael.fatesheets.adapter.ConsequenceArrayAdapter;
+import link.standen.michael.fatesheets.adapter.StressArrayAdapter;
 import link.standen.michael.fatesheets.model.Consequence;
 import link.standen.michael.fatesheets.model.CoreCharacter;
+import link.standen.michael.fatesheets.model.Stress;
 
 /**
  * A fragment for managing a characters stress.
@@ -23,6 +25,34 @@ public class CoreCharacterEditStressFragment extends CoreCharacterEditAbstractFr
 		View rootView = inflater.inflate(R.layout.core_character_edit_stress, container, false);
 
 		final CoreCharacter character = getCharacter();
+
+		// Physical Stress
+		final StressArrayAdapter physicalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
+				R.layout.core_character_edit_stress_list_item, getCharacter().getPhysicalStress());
+		((ListView) rootView.findViewById(R.id.physical_stress_list)).setAdapter(physicalStressListAdapter);
+
+		rootView.findViewById(R.id.add_physical_stress).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				int nextValue = character.getPhysicalStress().size() + 1;
+				character.getPhysicalStress().add(new Stress(nextValue));
+				physicalStressListAdapter.notifyDataSetChanged();
+			}
+		});
+
+		// Physical Stress
+		final StressArrayAdapter mentalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
+				R.layout.core_character_edit_stress_list_item, getCharacter().getMentalStress());
+		((ListView) rootView.findViewById(R.id.mental_stress_list)).setAdapter(mentalStressListAdapter);
+
+		rootView.findViewById(R.id.add_mental_stress).setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				int nextValue = character.getMentalStress().size() + 1;
+				character.getMentalStress().add(new Stress(nextValue));
+				mentalStressListAdapter.notifyDataSetChanged();
+			}
+		});
 
 		// Consequences
 		final ConsequenceArrayAdapter consequenceListAdapter = new ConsequenceArrayAdapter((CoreCharacterEditActivity) getContext(),
