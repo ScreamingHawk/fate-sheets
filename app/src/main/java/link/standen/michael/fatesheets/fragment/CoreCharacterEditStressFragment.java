a7f40a47754b7d3d9d247daacb788a6591cc15b8
Michael Standen <screaminghawk@gmail.com>
2017-04-13 17:48:42 +1200

    Enabled scrolling on multiple list layouts

--- app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStressFragment.java
+++ app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStressFragment.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import link.standen.michael.fatesheets.R;
 import link.standen.michael.fatesheets.activity.CoreCharacterEditActivity;
 import link.standen.michael.fatesheets.adapter.ConsequenceArrayAdapter;
 import link.standen.michael.fatesheets.adapter.StressArrayAdapter;
+import link.standen.michael.fatesheets.layout.AdapterLinearLayout;
 import link.standen.michael.fatesheets.model.Consequence;
 import link.standen.michael.fatesheets.model.CoreCharacter;
 import link.standen.michael.fatesheets.model.Stress;
@@ -29,7 +29,7 @@ public class CoreCharacterEditStressFragment extends CoreCharacterEditAbstractFr
 		// Physical Stress
 		final StressArrayAdapter physicalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
 				R.layout.core_character_edit_stress_list_item, getCharacter().getPhysicalStress());
-		((ListView) rootView.findViewById(R.id.physical_stress_list)).setAdapter(physicalStressListAdapter);
+		((AdapterLinearLayout) rootView.findViewById(R.id.physical_stress_list)).setAdapter(physicalStressListAdapter);
 
 		rootView.findViewById(R.id.add_physical_stress).setOnClickListener(new View.OnClickListener(){
 			@Override
@@ -43,7 +43,7 @@ public class CoreCharacterEditStressFragment extends CoreCharacterEditAbstractFr
 		// Physical Stress
 		final StressArrayAdapter mentalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
 				R.layout.core_character_edit_stress_list_item, getCharacter().getMentalStress());
-		((ListView) rootView.findViewById(R.id.mental_stress_list)).setAdapter(mentalStressListAdapter);
+		((AdapterLinearLayout) rootView.findViewById(R.id.mental_stress_list)).setAdapter(mentalStressListAdapter);
 
 		rootView.findViewById(R.id.add_mental_stress).setOnClickListener(new View.OnClickListener(){
 			@Override
@@ -57,7 +57,7 @@ public class CoreCharacterEditStressFragment extends CoreCharacterEditAbstractFr
 		// Consequences
 		final ConsequenceArrayAdapter consequenceListAdapter = new ConsequenceArrayAdapter((CoreCharacterEditActivity) getContext(),
 				R.layout.core_character_edit_consequence_list_item, getCharacter().getConsequences());
-		((ListView) rootView.findViewById(R.id.consequence_list)).setAdapter(consequenceListAdapter);
+		((AdapterLinearLayout) rootView.findViewById(R.id.consequence_list)).setAdapter(consequenceListAdapter);
 
 		rootView.findViewById(R.id.add_consequence).setOnClickListener(new View.OnClickListener(){
 			@Override
---
+++ app/src/main/java/link/standen/michael/fatesheets/layout/AdapterLinearLayout.java
@@ -0,0 +1,72 @@
+package link.standen.michael.fatesheets.layout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
+
+/**
+ * A linear layout that will contain views taken from an adapter. It differs
+ * from the list view in the fact that it will not optimize anything and
+ * draw all the views from the adapter. It also does not provide scrolling.
+ * However, when you need a layout that will render views horizontally and
+ * you know there are not many child views, this is a good option.
+
+ *
+ * @author Vincent Mimoun-Prat @ MarvinLabs
+ */
+public class AdapterLinearLayout extends LinearLayout {
+
+	private Adapter adapter;
+	private DataSetObserver dataSetObserver = new DataSetObserver() {
+		@Override
+		public void onChanged() {
+			super.onChanged();
+			reloadChildViews();
+		}
+	};
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	public AdapterLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public AdapterLinearLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public AdapterLinearLayout(Context context) {
+		super(context);
+	}
+
+	public void setAdapter(Adapter adapter) {
+		if (this.adapter == adapter) return;
+		this.adapter = adapter;
+		if (adapter != null) adapter.registerDataSetObserver(dataSetObserver);
+		reloadChildViews();
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		if (adapter != null) adapter.unregisterDataSetObserver(dataSetObserver);
+	}
+
+	private void reloadChildViews() {
+		removeAllViews();
+
+		if (adapter == null) return;
+
+		int count = adapter.getCount();
+		for (int position = 0; position < count; ++position) {
+			View v = adapter.getView(position, null, this);
+			if (v != null) addView(v);
+		}
+
+		requestLayout();
+	}
+}
