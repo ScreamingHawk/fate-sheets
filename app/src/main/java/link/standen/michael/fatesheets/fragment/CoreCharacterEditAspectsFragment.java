914391f17cbbc5b05ea9b41bcc331b7e9f8664ef
Michael Standen <screaminghawk@gmail.com>
2017-04-13 15:17:18 +1200

    Fixed whitespace

--- app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditAspectsFragment.java
+++ app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditAspectsFragment.java
@@ -22,7 +22,9 @@ public class CoreCharacterEditAspectsFragment extends CoreCharacterEditAbstractF
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.core_character_edit_aspects, container, false);final CoreCharacter character = getCharacter();
+		View rootView = inflater.inflate(R.layout.core_character_edit_aspects, container, false);
+
+		final CoreCharacter character = getCharacter();
 
 		// High Concept
 		TextView view = (TextView) rootView.findViewById(R.id.high_concept);
