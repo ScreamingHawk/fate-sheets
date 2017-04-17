b43a4f4934b5b6828a5f262c0f874f59c7b978d5
Michael Standen <screaminghawk@gmail.com>
2017-04-18 09:19:15 +1200

    Further compartmentalise the stress sections to prevent data corruption while maintaining list operations on dynamically sized lists

--- app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStressFragment.java
+++ app/src/main/java/link/standen/michael/fatesheets/fragment/CoreCharacterEditStressFragment.java
@@ -1,6 +1,8 @@
 package link.standen.michael.fatesheets.fragment;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,52 +24,114 @@ public class CoreCharacterEditStressFragment extends CoreCharacterEditAbstractFr
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.core_character_edit_stress, container, false);
+		return inflater.inflate(R.layout.core_character_edit_stress, container, false);
+	}
 
-		final CoreCharacter character = getCharacter();
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// Physical
+		Fragment childFragment = new PhysicalStressFragment();
+		FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+		transaction.replace(R.id.physical_stress_container, childFragment).commit();
+		// Mental
+		childFragment = new MentalStressFragment();
+		transaction = getChildFragmentManager().beginTransaction();
+		transaction.replace(R.id.mental_stress_container, childFragment).commit();
+		// Consequence
+		childFragment = new ConsequenceFragment();
+		transaction = getChildFragmentManager().beginTransaction();
+		transaction.replace(R.id.consequence_container, childFragment).commit();
+	}
 
-		// Physical Stress
-		final StressArrayAdapter physicalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
-				R.layout.core_character_edit_stress_list_item, getCharacter().getPhysicalStress());
-		((AdapterLinearLayout) rootView.findViewById(R.id.physical_stress_list)).setAdapter(physicalStressListAdapter);
+	/**
+	 * Class for managing physical stress.
+	 */
+	public static class PhysicalStressFragment extends CoreCharacterEditAbstractFragment {
 
-		rootView.findViewById(R.id.add_physical_stress).setOnClickListener(new View.OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				int nextValue = character.getPhysicalStress().size() + 1;
-				character.getPhysicalStress().add(new Stress(nextValue));
-				physicalStressListAdapter.notifyDataSetChanged();
-			}
-		});
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState) {
 
-		// Physical Stress
-		final StressArrayAdapter mentalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
-				R.layout.core_character_edit_stress_list_item, getCharacter().getMentalStress());
-		((AdapterLinearLayout) rootView.findViewById(R.id.mental_stress_list)).setAdapter(mentalStressListAdapter);
+			final CoreCharacter character = getCharacter();
 
-		rootView.findViewById(R.id.add_mental_stress).setOnClickListener(new View.OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				int nextValue = character.getMentalStress().size() + 1;
-				character.getMentalStress().add(new Stress(nextValue));
-				mentalStressListAdapter.notifyDataSetChanged();
-			}
-		});
+			View rootView = inflater.inflate(R.layout.core_character_edit_stress_physical, container, false);
 
-		// Consequences
-		final ConsequenceArrayAdapter consequenceListAdapter = new ConsequenceArrayAdapter((CoreCharacterEditActivity) getContext(),
-				R.layout.core_character_edit_consequence_list_item, getCharacter().getConsequences());
-		((AdapterLinearLayout) rootView.findViewById(R.id.consequence_list)).setAdapter(consequenceListAdapter);
+			// Physical Stress
+			final StressArrayAdapter physicalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
+					R.layout.core_character_edit_stress_list_item, getCharacter().getPhysicalStress());
+			((AdapterLinearLayout) rootView.findViewById(R.id.physical_stress_list)).setAdapter(physicalStressListAdapter);
 
-		rootView.findViewById(R.id.add_consequence).setOnClickListener(new View.OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				int nextValue = character.getConsequences().size() * 2 + 2;
-				character.getConsequences().add(new Consequence(nextValue));
-				consequenceListAdapter.notifyDataSetChanged();
-			}
-		});
+			rootView.findViewById(R.id.add_physical_stress).setOnClickListener(new View.OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					int nextValue = character.getPhysicalStress().size() + 1;
+					character.getPhysicalStress().add(new Stress(nextValue));
+					physicalStressListAdapter.notifyDataSetChanged();
+				}
+			});
 
-		return rootView;
+			return rootView;
+		}
+	}
+
+	/**
+	 * Class for managing mental stress.
+	 */
+	public static class MentalStressFragment extends CoreCharacterEditAbstractFragment {
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState) {
+
+			final CoreCharacter character = getCharacter();
+
+			View rootView = inflater.inflate(R.layout.core_character_edit_stress_mental, container, false);
+
+			final StressArrayAdapter mentalStressListAdapter = new StressArrayAdapter((CoreCharacterEditActivity) getContext(),
+					R.layout.core_character_edit_stress_list_item, getCharacter().getMentalStress());
+			((AdapterLinearLayout) rootView.findViewById(R.id.mental_stress_list)).setAdapter(mentalStressListAdapter);
+
+			rootView.findViewById(R.id.add_mental_stress).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					int nextValue = character.getMentalStress().size() + 1;
+					character.getMentalStress().add(new Stress(nextValue));
+					mentalStressListAdapter.notifyDataSetChanged();
+				}
+			});
+
+			return rootView;
+		}
+	}
+
+	/**
+	 * Class for managing consequences.
+	 */
+	public static class ConsequenceFragment extends CoreCharacterEditAbstractFragment {
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState) {
+
+			final CoreCharacter character = getCharacter();
+
+			View rootView = inflater.inflate(R.layout.core_character_edit_stress_consequence, container, false);
+
+			// Consequences
+			final ConsequenceArrayAdapter consequenceListAdapter = new ConsequenceArrayAdapter((CoreCharacterEditActivity) getContext(),
+					R.layout.core_character_edit_consequence_list_item, getCharacter().getConsequences());
+			((AdapterLinearLayout) rootView.findViewById(R.id.consequence_list)).setAdapter(consequenceListAdapter);
+
+			rootView.findViewById(R.id.add_consequence).setOnClickListener(new View.OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					int nextValue = character.getConsequences().size() * 2 + 2;
+					character.getConsequences().add(new Consequence(nextValue));
+					consequenceListAdapter.notifyDataSetChanged();
+				}
+			});
+
+			return rootView;
+		}
 	}
 }
