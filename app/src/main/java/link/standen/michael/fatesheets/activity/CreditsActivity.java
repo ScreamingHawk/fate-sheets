7a7e685624d1f7edb51224f3e4d4636e085ba888
Michael Standen <screaminghawk@gmail.com>
2017-05-11 07:17:31 +1200

    Add version to credits page

--- app/src/main/java/link/standen/michael/fatesheets/activity/CreditsActivity.java
+++ app/src/main/java/link/standen/michael/fatesheets/activity/CreditsActivity.java
@@ -1,8 +1,11 @@
 package link.standen.michael.fatesheets.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.widget.TextView;
 
 import link.standen.michael.fatesheets.R;
@@ -12,10 +15,20 @@ import link.standen.michael.fatesheets.R;
  */
 public class CreditsActivity extends AppCompatActivity {
 
+	private static final String TAG = CreditsActivity.class.getName();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.credits_activity);
+
+		// Version
+		try {
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			((TextView)findViewById(R.id.credits_version)).setText("v"+pInfo.versionName);
+		} catch (PackageManager.NameNotFoundException e) {
+			Log.e(TAG, "Unable to get package version", e);
+		}
 
 		// Linkify
 		((TextView)findViewById(R.id.credits_creator)).setMovementMethod(LinkMovementMethod.getInstance());
