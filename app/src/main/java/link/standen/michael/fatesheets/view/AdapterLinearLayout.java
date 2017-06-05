dfc96fd27e1c5324987344a298d228a73d2b4133
Michael Standen <screaminghawk@gmail.com>
2017-06-06 08:08:32 +1200

    #21 Clean up unneeded methods

--- app/src/main/java/link/standen/michael/fatesheets/view/AdapterLinearLayout.java
+++ app/src/main/java/link/standen/michael/fatesheets/view/AdapterLinearLayout.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.DataSetObserver;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.LinearLayout;
--- app/src/main/java/link/standen/michael/fatesheets/view/SingleChangeListenerEditText.java
+++ app/src/main/java/link/standen/michael/fatesheets/view/SingleChangeListenerEditText.java
@@ -1,18 +1,10 @@
 package link.standen.michael.fatesheets.view;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.widget.AppCompatEditText;
-import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * An extension of the EditText view that adds the ability to clear all textChangeListeners.
@@ -21,31 +13,16 @@ public class SingleChangeListenerEditText extends AppCompatEditText {
 
 	private TextWatcher mWatcher = null;
 
-	private final OnAttachStateChangeListener stateChangeListener = new OnAttachStateChangeListener() {
-		@Override
-		public void onViewAttachedToWindow(View v) {}
-
-		@Override
-		public void onViewDetachedFromWindow(View v) {
-			if (v != null && v instanceof SingleChangeListenerEditText){
-				((SingleChangeListenerEditText) v).clearTextChangedListeners();
-			}
-		}
-	};
-
 	public SingleChangeListenerEditText(Context context){
 		super(context);
-		addOnAttachStateChangeListener(stateChangeListener);
 	}
 
 	public SingleChangeListenerEditText(Context context, AttributeSet attrs){
 		super(context, attrs);
-		addOnAttachStateChangeListener(stateChangeListener);
 	}
 
 	public SingleChangeListenerEditText(Context context, AttributeSet attrs, int defStyle){
 		super(context, attrs, defStyle);
-		addOnAttachStateChangeListener(stateChangeListener);
 	}
 
 	@Override
@@ -81,12 +58,6 @@ public class SingleChangeListenerEditText extends AppCompatEditText {
 	public void onRestoreInstanceState(Parcelable state){
 		clearTextChangedListeners();
 		super.onRestoreInstanceState(state);
-	}
-
-	@Override
-	protected void onDetachedFromWindow(){
-		clearTextChangedListeners();
-		super.onDetachedFromWindow();
 	}
 
 }
